5cee3ce2236f9d305c078c6b3489b7d2dd2e53a2
VitaStain <100089853+VitaStain@users.noreply.github.com>
2022-11-08 15:20:05 +0300

    Update sql_1.sql

--- sql/sql_1.sql
+++ sql/sql_1.sql
@@ -1,5 +1,4 @@
-SELECT nb.title, COUNT(*) AS count FROM notebooks_notebook nn
-LEFT JOIN notebooks_brand nb ON nb.id = nn.brand_id
+SELECT nb.title, COUNT(nn.title) AS count FROM notebooks_notebook nn
+INNER JOIN notebooks_brand nb ON nb.id = nn.brand_id
 GROUP BY nb.title 
 ORDER BY count DESC;
-
